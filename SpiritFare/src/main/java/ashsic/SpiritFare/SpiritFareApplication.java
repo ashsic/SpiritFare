7b76d36c4143a9da0b0fa4ee55ff528ac789fdc1
ashsic <sicardashton@gmail.com>
2024-09-29 21:00:30 -0400

    init auth/user/etc. layer

---
+++ SpiritFare/src/main/java/ashsic/SpiritFare/SpiritFareApplication.java
@@ -0,0 +1,13 @@
+package ashsic.SpiritFare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpiritFareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpiritFareApplication.class, args);
+	}
+
+}
---
+++ SpiritFare/src/test/java/ashsic/SpiritFare/SpiritFareApplicationTests.java
@@ -0,0 +1,13 @@
+package ashsic.SpiritFare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpiritFareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
